43f29ff86a39037e6fff7432a8fa58f44bdf3c64
Philip Tasabia <tasabiap@oregonstate.edu>
2023-11-26 08:43:43 -0500

    update to use firebase instead of firebase-admin

--- mentor-matching/src/dal/menteeDb.ts
+++ mentor-matching/src/dal/menteeDb.ts
@@ -1,7 +1,9 @@
 import { db } from "./firebaseConfig";
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 async function readInterests() {
-  const interestsSnapshot = await db.collection("interests").get();
+  const interestsCollection = collection(db, "interests");
+  const interestsSnapshot = await getDocs(interestsCollection);
   const interests = interestsSnapshot.docs.map((doc) => doc.data());
   return interests;
 }
