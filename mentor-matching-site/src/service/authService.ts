2df680b4229eb3b3beeb2779827894210f242efc
Philip Tasabia <tasabiap@oregonstate.edu>
2023-11-27 18:36:28 -0500

    add login persistence

--- mentor-matching-site/src/service/authService.ts
+++ mentor-matching-site/src/service/authService.ts
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, setPersistence, browserSessionPersistence, getAuth } from "firebase/auth";
 import { app } from '../firebaseConfig';
 
 const auth = getAuth(app)
@@ -7,7 +7,8 @@ function createUser(email: string, password: string) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed up 
-      // const user = userCredential.user;
+      const user = userCredential.user;
+      return user;
       // ...
     })
     .catch((error) => {
@@ -23,10 +24,28 @@ function signIn(email: string, password: string) {
       // Signed in 
       // const user = userCredential.user;
       // ...
+      persistLogin(email, password);
     })
     .catch((error) => {
       // const errorCode = error.code;
       // const errorMessage = error.message;
+    });
+}
+
+function persistLogin(email: string, password: string) {
+  setPersistence(auth, browserSessionPersistence)
+    .then(() => {
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      return signInWithEmailAndPassword(auth, email, password);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
     });
 }
 
