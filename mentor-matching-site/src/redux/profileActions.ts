f0b876adc68e67c49ab0a9625320aa01f8eabe32
Philip Tasabia <tasabiap@oregonstate.edu>
2023-12-05 19:31:13 -0500

    add types to actions

--- mentor-matching-site/src/redux/profileActions.ts
+++ mentor-matching-site/src/redux/profileActions.ts
@@ -1,10 +1,17 @@
 // Actions for profile management:
 
+import { Action } from "redux";
+import { UserProfile } from "../types";
+
+export interface UserProfileAction extends Action {
+  payload: UserProfile
+}
+
 // profileActions.js
-export const setUserProfile = (userProfile) => {
+export const setUserProfile = (userProfile: UserProfile) : UserProfileAction => {
     return {
       type: 'SET_USER_PROFILE',
       payload: userProfile,
-    };
+    } as UserProfileAction;
   };
   
--- mentor-matching-site/src/redux/reducers/profileReducer.ts
+++ mentor-matching-site/src/redux/reducers/profileReducer.ts
@@ -1,16 +1,17 @@
 // This reducer will handle the user profile state:
 
+import { UserProfileAction } from "../profileActions";
 
 const initialState = {
     userProfile: null,
   };
   
-  const profileReducer = (state = initialState, action) => {
+  const profileReducer = (state = initialState, action: UserProfileAction) => {
     switch (action.type) {
       case 'SET_USER_PROFILE':
         return {
           ...state,
-          userProfile: action.payload,
+          userProfile: action,
         };
       // handle other actions if necessary
       default:
