a1eaeb28cacbca421658a3a0b0b371bff48dd259
Philip T <69321457+philipscoderepo@users.noreply.github.com>
2023-12-19 21:19:16 -0500

    add loading message

--- mentor-matching-site/src/app/userProfile/ViewUserProfile.tsx
+++ mentor-matching-site/src/app/userProfile/ViewUserProfile.tsx
@@ -11,15 +11,6 @@ import TextDisplay from "../common/TextDisplay";
 import FormGroupRows from "../common/FormGroupRows";
 
 function ViewUserProfile() {
-  // const email = "p.a.tasabia@gmail.com";
-  // const displayName = "Philip T.";
-  // const firstName = "Philip";
-  // const lastName = "Tasabia";
-  // const middleName = "Andrew";
-  // const pronouns = "he/him";
-  // const racialIdentity = "Hispanic";
-  // const timeZone = "Eastern US";
-  // const userBio = "Hi! My name is philip and I am a senior in the OSU Computer Science program."
   const [userProfile, setUserProfile] = useState<UserProfile | undefined>(undefined);
   const navigate = useNavigate();
 
@@ -34,25 +25,38 @@ function ViewUserProfile() {
     loadUserProfile();
   }, []);
 
+  const dataIsLoading = () => {
+    if (userProfile === undefined) {
+      return (<>Data is loading...</>)
+    }
+    else {
+      return dataDisplay;
+    }
+  }
+
+  const dataDisplay = (
+    <FormGroupCols>
+      <FormGroupRows>
+        <TextDisplay value={userProfile?.personal.firstName} label="First Name" />
+        <TextDisplay value={userProfile?.personal.lastName} label="Last Name" />
+      </FormGroupRows>
+      <FormGroupRows>
+        <TextDisplay value={userProfile?.contact.email} label="Email" />
+        <TextDisplay value={userProfile?.contact.displayName} label="Display Name" />
+        <TextDisplay value={userProfile?.contact.pronouns} label="Pronouns" />
+        <TextDisplay value={userProfile?.contact.timeZone} label="Time Zone" />
+      </FormGroupRows>
+      <FormGroupRows>
+        <TextDisplay value={userProfile?.demographics.lgbtqPlusCommunity ? 'Yes' : 'No'} label="Identify as LGBTQ+" />
+        <TextDisplay value={userProfile?.demographics.racialIdentity} label="Racial Identity" />
+      </FormGroupRows>
+      <Button onClick={() => navigate('/profile/edit')}>Edit Profile</Button>
+    </FormGroupCols>
+  );
+
   return (
     <div className="user-profile">
-      <FormGroupCols>
-        <FormGroupRows>
-          <TextDisplay value={userProfile?.personal.firstName} label="First Name" />
-          <TextDisplay value={userProfile?.personal.lastName} label="Last Name" />
-        </FormGroupRows>
-        <FormGroupRows>
-          <TextDisplay value={userProfile?.contact.email} label="Email" />
-          <TextDisplay value={userProfile?.contact.displayName} label="Display Name" />
-          <TextDisplay value={userProfile?.contact.pronouns} label="Pronouns" />
-          <TextDisplay value={userProfile?.contact.timeZone} label="Time Zone" />
-        </FormGroupRows>
-        <FormGroupRows>
-          <TextDisplay value={userProfile?.demographics.lgbtqPlusCommunity ? 'Yes' : 'No'} label="Identify as LGBTQ+" />
-          <TextDisplay value={userProfile?.demographics.racialIdentity} label="Racial Identity" />
-        </FormGroupRows>
-        <Button onClick={() => navigate('/profile/edit')}>Edit Profile</Button>
-      </FormGroupCols>
+      {dataIsLoading()}
     </div>
   );
 }
