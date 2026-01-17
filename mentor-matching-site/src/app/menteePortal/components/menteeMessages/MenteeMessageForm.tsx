a956aaf8d6d0450d14e7af58df2f6fe2661f64c9
simmocha <simmocha@oregonstate.edu>
2026-01-16 22:28:50 -0800

    Added form skeleton for messages
    Form now has fields for adding a recipient and a message.  Both are simple dummy forms for now, but it is processing the input.  You can see this in the browser console after clicking the button.

--- mentor-matching-site/src/app/menteePortal/components/menteeMessages/MenteeMessageForm.tsx
+++ mentor-matching-site/src/app/menteePortal/components/menteeMessages/MenteeMessageForm.tsx
@@ -5,26 +5,47 @@ import { DocItem } from "../../../../types/types";
 import { Message } from "../../../../types/matchProfile";
 import ContentContainer from "../../../common/ContentContainer";
 
-function SendMessageHandler(e: any) {
-    e.preventDefault();
-    console.log("Send message button pressed")
-}
-
-
 function MenteeMessageForm() {
 
+    const [messageDetails, setMessageDetails] = useState({
+        recipient: "",
+        message: ""
+    });
+
+    function sendMessageHandler(e: any) {
+        e.preventDefault();
+        console.log("Send message button pressed\n" + messageDetails.recipient + "\n" + messageDetails.message)
+    }
+
+    function changeMessageHandler(e: any) {
+        const inputName = e.target.name;
+        const inputValue = e.target.value;
+
+        setMessageDetails(values => ({...values, [inputName]: inputValue}))
+    }
 
     return (
         <ContentContainer>
             <form>
                 <label>
                     Recipient's Name:
+                    <input
+                        type="text"
+                        name="recipient"
+                        value={messageDetails.recipient}
+                        onChange={changeMessageHandler}
+                    />
                 </label>
-                <input
-                    type="text"
-                    name="recipient"
-                />
-                <input type="submit" onClick={SendMessageHandler} />
+                <label>
+                    Your Message:
+                    <input
+                        type="text"
+                        name="message"
+                        value={messageDetails.message}
+                        onChange={changeMessageHandler}
+                    />
+                </label>
+                <input type="submit" onClick={sendMessageHandler} />
             </form>
         </ContentContainer>
     );
