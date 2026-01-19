8a922a222fed8ea24afc6c9dc39dcb74b357cc60
simmocha <simmocha@oregonstate.edu>
2026-01-18 21:08:14 -0800

    Allow the user to select a recipient and send a message.
    It appears to actually send the message to the database and displays in the Messages section.

--- mentor-matching-site/src/app/menteePortal/components/menteeMessages/MenteeMessageForm.tsx
+++ mentor-matching-site/src/app/menteePortal/components/menteeMessages/MenteeMessageForm.tsx
@@ -14,7 +14,20 @@ function MenteeMessageForm() {
 
     function sendMessageHandler(e: any) {
         e.preventDefault();
-        console.log("Send message button pressed\n" + messageDetails.recipient + "\n" + messageDetails.message)
+        console.log("Send message button pressed\n" + messageDetails.recipient + "\n" + messageDetails.message);
+        let message = {
+            menteeUID: messageDetails.recipient,
+            menteeProfileId: "vSXEbCikuXO6kzSqI22Dmm27Lbh1",
+            mentorUID: "vSXEbCikuXO6kzSqI22Dmm27Lbh1",
+            mentorProfileId: "vSXEbCikuXO6kzSqI22Dmm27Lbh1",
+            message: messageDetails.message,
+            mentorReply: "vSXEbCikuXO6kzSqI22Dmm27Lbh1",
+            technicalInterest: "Debug",
+            professionalInterest: "Debug",
+            sentByUID: "vSXEbCikuXO6kzSqI22Dmm27Lbh1",
+            sentOn: 0
+        }
+        messagingService.sendMessage(message);
     }
 
     function changeMessageHandler(e: any) {
@@ -29,9 +42,9 @@ function MenteeMessageForm() {
             <form>
                 <h2>
                     DEBUG! Use these IDs in place of the user name for now.  This is just for testing the send/receive functions for now. <br />
-                    Charles: vSXEbCikuXO6kzSqI22Dmm27Lbh1 <br />
-                    Andrew: LsWSk3HnDJPIuKElzd7u0zw4LYj2 <br />
-                    Aimee: iqB5Og7bTwSYhdnKRMNjNRQc6RC3 <br />
+                    Charles: vSXEbCikuXO6kzSqI22Dmm27Lbh1<br />
+                    Andrew: LsWSk3HnDJPIuKElzd7u0zw4LYj2<br />
+                    Aimee: iqB5Og7bTwSYhdnKRMNjNRQc6RC3<br />
                 </h2>
                 <label>
                     Recipient's Name:
@@ -42,16 +55,16 @@ function MenteeMessageForm() {
                         onChange={changeMessageHandler}
                     />
                 </label>
+                <br />
                 <label>
                     Your Message:
-                    <input
-                        type="text"
+                    <textarea
                         name="message"
                         value={messageDetails.message}
                         onChange={changeMessageHandler}
                     />
                 </label>
-                <input type="submit" onClick={sendMessageHandler} />
+                <input type="submit" onClick={sendMessageHandler} value="Send Message"/>
             </form>
         </ContentContainer>
     );
