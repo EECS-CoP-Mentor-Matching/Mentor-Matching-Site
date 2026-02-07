0f74c2130bea7202df1c6c26fe403109d2bcb968
simmocha <simmocha@oregonstate.edu>
2026-02-06 22:49:56 -0800

    Utilizing URL parameters to pass in the user ID
    Previously attempted to use Component Props to pull in the user ID, but it became troublesome as there was no way to put the user ID in as a parameter.  Instead, planning to put the user ID in the URL itself and then extract it when needed.

--- mentor-matching-site/src/app/adminPortal/components/manageUsers/ManageUserProfile.tsx
+++ mentor-matching-site/src/app/adminPortal/components/manageUsers/ManageUserProfile.tsx
@@ -1,10 +1,20 @@
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Button, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, FormGroup } from "@mui/material";
 
-function ManageUserProfile(userID: any) {
-    // Specify the user ID as a property/parameter above, and use it to pull the existing
-    // user details from the database so that we can populate the form.
+function ManageUserProfile() {
+    // Expects a user ID provided as part of the URL.  Will extract the ID
+    // and attempt to look the user up in our database to populate the
+    // edit user form.
 
+    const {userID} = useParams();
+
+    return (
+        <>
+            <h2>DEBUG</h2>
+            <p> Received user ID: {userID}</p>
+        </>
+    )
 
 }
 
