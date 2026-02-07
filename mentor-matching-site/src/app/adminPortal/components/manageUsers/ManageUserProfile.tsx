a79bb988a441f637bbec25a55ff50c71358545ac
simmocha <simmocha@oregonstate.edu>
2026-02-06 22:13:19 -0800

    Initial work on creating an admin form for editing user details
    This is the start of the Component for displaying/editing user details for admin users.  Currently planning to make it similar to the end-user's profile edit form.

---
+++ mentor-matching-site/src/app/adminPortal/components/manageUsers/ManageUserProfile.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import { Button, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, FormGroup } from "@mui/material";
+
+function ManageUserProfile(userID: any) {
+    // Specify the user ID as a property/parameter above, and use it to pull the existing
+    // user details from the database so that we can populate the form.
+
+
+}
+
+export default ManageUserProfile;
