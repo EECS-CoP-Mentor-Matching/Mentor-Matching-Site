7a03ba4593bc9e3cd8e86d66d0f1b464705ce019
simmocha <simmocha@oregonstate.edu>
2026-02-07 21:56:59 -0800

    Admin form: Pull user's profile from the database
    In the edit user form for admins, attempting to pull the user's profile from the database.

--- mentor-matching-site/src/app/adminPortal/components/manageUsers/ManageUserProfile.tsx
+++ mentor-matching-site/src/app/adminPortal/components/manageUsers/ManageUserProfile.tsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Button, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, FormGroup } from "@mui/material";
+import userService from '../../../../service/userService';
+import { UserProfile } from '../../../../types/userProfile';
 
 function ManageUserProfile() {
     // Expects a user ID provided as part of the URL.  Will extract the ID
@@ -8,6 +10,18 @@ function ManageUserProfile() {
     // edit user form.
 
     const {userID} = useParams();
+    let userProfile: UserProfile | null | undefined = undefined;
+
+    useEffect(() => {
+        const getUserProfile = async () => {
+            if (userID)
+            {
+                userProfile = await userService.getUserProfile(userID);
+            }
+        };
+    }, []);
+
+    
 
     return (
         <>
