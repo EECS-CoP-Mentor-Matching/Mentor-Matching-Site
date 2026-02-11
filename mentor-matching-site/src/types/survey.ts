8abbd1c14447fd2757101c16c5f772f21ef17c4c
wongaokay <23andrewwong@gmail.com>
2026-02-10 18:13:10 -0800

    created survey types for type safety

---
+++ mentor-matching-site/src/types/survey.ts
@@ -0,0 +1,31 @@
+export interface SurveySchema {
+  title: string
+  version: number
+}
+
+export interface SurveyQuestion {
+    algorithmWeight: 0 | 1 | 2 | 3 | 4 | 5
+    displayUi: DisplayUi
+    optionFreeResponse: boolean
+    optionOptIn: boolean
+    prompts: PromptsDict
+    required: boolean
+    status: boolean
+}
+
+type PromptsDict = {
+    "mentee": string
+    "mentor": string
+}
+
+enum DisplayUi {
+    checkbox = "checkbox",
+    radio = "radio",
+    dropdown = "dropdown"
+}
+
+export interface SurveyOption {
+    QID: string
+    label: string
+    status: boolean
+}
