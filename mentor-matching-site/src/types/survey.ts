6edd97559d62c0ca1907db3c332f338364f809bd
wongaokay <23andrewwong@gmail.com>
2026-02-12 14:35:12 -0800

    survey schema

---
+++ mentor-matching-site/src/types/survey.ts
@@ -0,0 +1,32 @@
+export interface SurveySchema {
+  title: string
+  version: number
+  isCurrentSchema: boolean
+}
+
+export interface SurveyQuestion {
+    algorithmWeight: 0 | 1 | 2 | 3 | 4 | 5
+    displayUi: DisplayUi
+    optionFreeResponse: boolean
+    optionOptIn: boolean
+    prompts: PromptsDict
+    required: boolean
+    status: boolean
+    options?: SurveyOption[]
+}
+
+type PromptsDict = {
+    "mentee": string
+    "mentor": string
+}
+
+enum DisplayUi {
+    checkbox = "checkbox",
+    radio = "radio",
+    dropdown = "dropdown"
+}
+
+export interface SurveyOption {
+    label: string
+    status: boolean
+}
