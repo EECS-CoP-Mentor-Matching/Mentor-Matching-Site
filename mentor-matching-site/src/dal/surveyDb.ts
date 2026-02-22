d2f91314837845c63a2048e7ae61d41bd012b849
wongaokay <23andrewwong@gmail.com>
2026-02-21 21:02:05 -0800

    Adding service calls for computing comapatibility

--- mentor-matching-site/src/dal/surveyDb.ts
+++ mentor-matching-site/src/dal/surveyDb.ts
@@ -1,6 +1,6 @@
-import { queryMany, querySingle, readMany, readSingle, writeSingle} from "./commonDb";
+import { queryDocId, queryMany, querySingle, readMany, readSingle, writeSingle} from "./commonDb";
 import { DocItem, DbReadResult, DbReadResults, DbWriteResult } from "../types/types";
-import { Option, Question, SurveyResponse, SurveySchema } from "../types/survey";
+import { Option, Question, SurveyResponse, SurveySchema, CompatibilityScoreResult } from "../types/survey";
 import { app, db } from "../firebaseConfig";
 import { collection, getDocs, doc, query, where, setDoc, updateDoc, deleteDoc, getFirestore, orderBy } from "firebase/firestore";
 
@@ -36,10 +36,55 @@ async function createSurveyResponseAsync(uid: string, role: "mentee" | "mentor",
   return result;
 }
 
+async function getAllMentorSurveyRespAsync(): Promise<DocItem<SurveyResponse>[]> {
+  const responses = await queryMany<SurveyResponse>(resultsCollectionName, where("role", "==", "mentor"));
+  return responses.results;
+}
+
+async function getAllMenteeSurveyRespAsync(): Promise<DocItem<SurveyResponse>[]> {
+  const responses = await queryMany<SurveyResponse>(resultsCollectionName, where("role", "==", "mentee"));
+  return responses.results;
+}
+
+// Score calcualted with Jaccard method
+// score = |intersection| / |union|
+async function computeCompatibilityAsync(menteeSurveyRespId: string, mentorSurveyRespId: string): Promise<CompatibilityScoreResult> {
+
+  // Find the intersection of dict keys
+  function intersection(o1: Record<string, any>, o2: Record<string, any>): string[] {
+    const [k1, k2] = [Object.keys(o1), Object.keys(o2)];
+    const [first, next] = k1.length > k2.length ? [k2, o1] : [k1, o2];
+    return first.filter(k => k in next);
+  }
+
+  function jaccard_index(s1: Record<string, any>, s2: Record<string, any>): number {
+    let size_s1 = s1.length;
+    let size_s2 = s2.length;
+
+    let intersect = intersection(s1, s2);
+    let size_in = intersect.length;
+    let jaccard_in = size_in / (size_s1 + size_s2 - size_in);
+
+    return jaccard_in;
+  }
+
+  const menteeRespDoc: DbReadResult<SurveyResponse> = await queryDocId(resultsCollectionName, menteeSurveyRespId);
+  const mentorRespDoc: DbReadResult<SurveyResponse> = await queryDocId(resultsCollectionName, mentorSurveyRespId);
+  const score = jaccard_index(menteeRespDoc.data, mentorRespDoc.data);
+  
+  const result: CompatibilityScoreResult = {
+    menteeSurveyRespId,
+    mentorSurveyRespId,
+    score
+  }
+  return result;
+}
+
 const surveyDb = {
   getAllQuestionsAsync,
   createQuestionAsync,
-  createSurveyResponseAsync
+  createSurveyResponseAsync,
+  computeCompatibilityAsync
 }
 
 export default surveyDb
