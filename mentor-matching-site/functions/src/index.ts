608d6d9c01c73fdb7a5ebcb475cb6f6af26ef2be
simmocha <simmocha@oregonstate.edu>
2026-02-26 23:22:22 -0800

    Init Firebase Functions
    As per CoPilot, this is needed in order to run code server-side to mark pending user's accounts as approved.
    
    https://copilot.microsoft.com/shares/CGHZiEpgKk8FzDxD2x9PR

---
+++ mentor-matching-site/functions/src/index.ts
@@ -0,0 +1,39 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+import { onCall } from "firebase-functions/v2/https";
+import * as adminFunctions from "firebase-admin";
+
+adminFunctions.initializeApp();
+
+// Function for approving a user who does not have an OSU email address:
+export const approvePendingUser = onCall(async (request) => {
+  const userData = request.data;
+
+  if (!request.auth) {
+    throw new Error("You are not signed in.  Please sign in first.");
+  }
+
+  if (request.auth.token.admin !== true) {
+    throw new Error("This function can be run by authorized Mentor Match Admins only.");
+  }
+
+  const uid = userData.uid;
+  if (!uid) {
+    throw new Error("User ID (UID) not provided.");
+  }
+
+  await adminFunctions.auth().setCustomUserClaims(uid, { allowed: true });
+  await adminFunctions.firestore().doc(`pendingUsers/${uid}`).delete();
+
+  return { success: true };
+});
