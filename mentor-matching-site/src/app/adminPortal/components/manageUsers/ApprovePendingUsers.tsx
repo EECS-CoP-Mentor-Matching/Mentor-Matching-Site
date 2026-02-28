3e1cb94062063d6bffb4ea11b016f735b5df4b46
simmocha <simmocha@oregonstate.edu>
2026-02-27 17:56:28 -0800

    Create rows in our table with user's email and timestamp
    Email address and timestamp are getting pulled in.  Next we need a button to approve the user.

--- mentor-matching-site/src/app/adminPortal/components/manageUsers/ApprovePendingUsers.tsx
+++ mentor-matching-site/src/app/adminPortal/components/manageUsers/ApprovePendingUsers.tsx
@@ -1,6 +1,8 @@
 import {getFunctions, httpsCallable} from "firebase/functions";
 import { useEffect, useState } from "react";
 import userService from "../../../../service/userService";
+import { Box, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Timestamp } from "firebase/firestore";
 
 const adminFunctions = getFunctions();
 const approvePendingUser = httpsCallable(getFunctions(), "approvePendingUser");
@@ -30,10 +32,31 @@ function ApprovePendingUsers()
 
             DEBUG!  A list of pending users with the option to approve them will be coming soon!
             {pendingUserList && pendingUserList.map((pendingUser: any) => 
-                <p>{pendingUser.uid}: {pendingUser.details.email}</p>
-                
+                    <p>{pendingUser.uid}: {pendingUser.details.email}</p>
                 )
             };
+            <Box sx={{
+                maxWidth: 750,
+                margin: "30px auto",
+                padding: "10px"
+                }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Timestamp</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>{pendingUserList.map((pendingUser) => (
+                        <TableRow key={pendingUser.uid}>
+                            <TableCell>{pendingUser.details.email}</TableCell>
+                            <TableCell>{Timestamp.fromMillis(pendingUser.details.createdAt.seconds*1000).toDate().toDateString()}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </Box>
         </>
     );
 }
