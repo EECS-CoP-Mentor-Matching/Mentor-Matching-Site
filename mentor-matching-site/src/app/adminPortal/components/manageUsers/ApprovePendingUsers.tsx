3317507e298f13d84ab1d79380542427318420da
simmocha <simmocha@oregonstate.edu>
2026-02-27 18:52:08 -0800

    Initial setup on the Approve button
    Button has some initial wiring for the approval process ,but is missing some functionality (such as deleting the pendingUser object from the database)

--- mentor-matching-site/src/app/adminPortal/components/manageUsers/ApprovePendingUsers.tsx
+++ mentor-matching-site/src/app/adminPortal/components/manageUsers/ApprovePendingUsers.tsx
@@ -1,7 +1,7 @@
 import {getFunctions, httpsCallable} from "firebase/functions";
 import { useEffect, useState } from "react";
 import userService from "../../../../service/userService";
-import { Box, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Box, Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import { Timestamp } from "firebase/firestore";
 
 const adminFunctions = getFunctions();
@@ -26,6 +26,19 @@ function ApprovePendingUsers()
     }, []);
 
     console.log(pendingUserList);
+
+    async function handleApproveUser(uid: string) {
+        // Function to handle the click for the approe button.  Takes the user's ID and marks them as approved in Firebase.
+        try {
+            const approveResult = await approvePendingUser(uid);
+            // Log the results for debugging purposes:
+            console.log("Approval result: ", approveResult.data);
+        }
+        catch (error) {
+            console.log("Error approving user: ", error);
+        }
+
+    }
 
     return(
         <>
@@ -52,6 +65,7 @@ function ApprovePendingUsers()
                         <TableRow key={pendingUser.uid}>
                             <TableCell>{pendingUser.details.email}</TableCell>
                             <TableCell>{Timestamp.fromMillis(pendingUser.details.createdAt.seconds*1000).toDate().toDateString()}</TableCell>
+                            <TableCell><Button>Approve</Button></TableCell>
                         </TableRow>
                     ))}
                     </TableBody>
