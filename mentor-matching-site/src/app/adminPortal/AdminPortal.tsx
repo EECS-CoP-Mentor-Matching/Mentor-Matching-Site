7df66e070aa9087fac682422f8dfa33013334f78
wongaokay <23andrewwong@gmail.com>
2026-05-01 17:29:35 -0700

    removed unnecessary nested folders for admin feedback

--- mentor-matching-site/src/app/adminPortal/AdminPortal.tsx
+++ mentor-matching-site/src/app/adminPortal/AdminPortal.tsx
@@ -10,7 +10,7 @@ import Settings from './components/settings/Settings';
 import TestingPanel from './components/testing/TestingPanel';
 import SendEmailForm from './components/sendEmail/SendEmailForm';
 
-import { FeedbackSettingsContext } from './components/FeedbackAdminPortal/submitFeedback/FeedbackSettingsContext';
+import { FeedbackSettingsContext } from './components/FeedbackAdminPortal/FeedbackSettingsContext';
 import { getSettings } from '../../service/settingsService';
 
 export enum Pages {
